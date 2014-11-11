c607d13201d0309c3fe29e1767954874ff1a9a2a
Ashley <saeshyls@gmail.com>
2014-11-11 09:09:58 -0500

    SQL cleanup

--- sql/struct/logs.sql
+++ sql/struct/logs.sql
@@ -1,15 +1,15 @@
 /*
-SQLyog Community v11.3 (64 bit)
-MySQL - 5.6.14-62.0 : Database - logs
+SQLyog Community v11.31 (64 bit)
+MySQL - 5.6.16 : Database - logs
 *********************************************************************
-*/
-
+*/
 
 /*!40101 SET NAMES utf8 */;
 
 /*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 /*Table structure for table `chat_logs` */
@@ -25,8 +25,6 @@ CREATE TABLE `chat_logs` (
   `type` int(3) NOT NULL DEFAULT '0',
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*Data for the table `chat_logs` */
 
 /*Table structure for table `command_logs` */
 
@@ -67,5 +65,6 @@ CREATE TABLE `trade_logs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
