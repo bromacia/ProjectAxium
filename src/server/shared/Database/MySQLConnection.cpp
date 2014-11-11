612db1f25f8627adc78803c34c0a3ec90ba002f1
Ashley <saeshyls@gmail.com>
2014-11-11 09:10:33 -0500

    Remove old MySQL error message
     - Bacon :(

--- src/server/shared/Database/MySQLConnection.cpp
+++ src/server/shared/Database/MySQLConnection.cpp
@@ -499,7 +499,6 @@ bool MySQLConnection::_HandleMySQLErrno(uint32 errNo)
         // Outdated table or database structure - terminate core
         case 1054:      // "Unknown column '%s' in '%s'"
         case 1146:      // "Table '%s' doesn't exist"
-            WPFatal(!errNo, "Give me the damn updated database structure -Bacon");
             return false;
 
         default:
